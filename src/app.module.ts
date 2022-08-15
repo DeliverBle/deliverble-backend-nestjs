d02b257cc7ed0682b9b2fd0807e319061a0cf132
Woojin Lee <dhkep03@gmail.com>
2022-08-15 13:40:01 +0900

    [FEAT] Auth provider start

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { NewsModule } from './news/news.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
     NewsModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
