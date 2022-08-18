185ceb422f05611bb0e7b1315005bbfe30f22850
Woojin Lee <dhkep03@gmail.com>
2022-08-19 00:51:22 +0900

    [FEAT] Kakao login practice

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { KakaoStrategy } from './kakao.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, KakaoStrategy]
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const KakaoStrategy = require('passport-kakao').Strategy;
+const passport = require('passport')
+const kakaoClientId = process.env.KAKAO_CLIENT_ID;
+const kakaoCallbackURL = process.env.KAKAO_CALLBACK_URL;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
+
+passport.use('kakao', new KakaoStrategy({
+  clientID: kakaoClientId,
+  callbackURL: kakaoCallbackURL,     // 위에서 설정한 Redirect URI
+}, async (accessToken, refreshToken, profile, done) => {
+  //console.log(profile);
+  console.log(accessToken);
+  console.log(refreshToken);
+}))
