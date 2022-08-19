90ff57925aa80137e4111cadb4cfa2b5ca86c075
Woojin Lee <dhkep03@gmail.com>
2022-08-20 02:57:14 +0900

    [CHORE] inject typeorm module in auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { KakaoStrategy } from './kakao.strategy';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, KakaoStrategy]
 })
 export class AuthModule {}
+
