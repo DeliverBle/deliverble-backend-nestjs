9a9cbfef55843c2dd31e3947e3b0ad12b8e9542f
Woojin Lee <dhkep03@gmail.com>
2022-09-24 22:59:03 +0900

    refactor: create new response rule

---
+++ src/modules/response/response.message.ts
@@ -0,0 +1,28 @@
+export const message = {
+  NULL_VALUE: '필요한 값이 없습니다.',
+  FORBIDDEN: 'Forbidden',
+  DUPLICATED: 'Duplicated',
+  NOT_FOUND: '존재하지 않는 자원',
+  BAD_REQUEST: '잘못된 요청',
+  INTERNAL_SERVER_ERROR: '서버 내부 오류',
+  NULL_VALUE_TOKEN: '토큰이 없습니다.',
+  INVALID_TOKEN: '만료된 토큰입니다.',
+  INVALID_PASSWORD: '비밀번호 오류',
+
+  READ_USER_SUCCESS: '유저 조회 성공',
+  CREATE_USER_SUCCESS: '유저 생성 성공',
+  DELETE_USER_SUCCESS: '유저 삭제 성공',
+  UPDATE_USER_SUCCEss: '유저 삭제 성공',
+  
+  // 인증, 인가 로직
+  AUTHENTICATION_SUCCESS: '유저 인증 성공',
+
+  // 리뷰
+  CREATE_REVIEW_SUCCESS: '리뷰 작성 성공',
+  READ_REVIEW_SUCCESS: '리뷰 조회 성공',
+
+  // 영화
+  CREATE_MOVIE_SUCESS: '영화 생성 성공',
+  CREATE_MOVIE_COMMENT_SUCCESS: '댓글 작성 성공',
+  READ_MOVIE_SUCEESS: '영화 조회 성공'
+}
---
+++ src/modules/response/response.util.ts
@@ -0,0 +1,18 @@
+export const util = {
+  success: (status: number, message: string, data?: any) => {
+      return {
+          status,
+          success: true,
+          message,
+          data,
+      };
+  },
+  fail: (status: number, message: string, data?: any) => {
+      return {
+          status,
+          success: false,
+          message,
+          data
+      };
+  },
+};
