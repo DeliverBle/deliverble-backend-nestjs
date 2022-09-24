5fbe1c8729e011a5e6831c7763a57f09b1665023
Woojin Lee <dhkep03@gmail.com>
2022-09-25 00:13:12 +0900

    refactor: adjust new response rule on getUserInfo

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Logger, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { User } from 'src/user/user.entity';
 import { UserForViewDto } from './dto/user-for-view.dto';
 import { ReturnUserDto } from './dto/return-user.dto';
 import { UserService } from './user.service';
+import { statusCode } from 'src/modules/response/response.status.code';
+import { util } from 'src/modules/response/response.util';
+import { message } from 'src/modules/response/response.message';
+
+const logger: Logger = new Logger('user controller')
+
 
 @Controller('user')
 export class UserController {
@@ -11,9 +17,27 @@ export class UserController {
 
   @Get('')
   @UseGuards(JwtAuthGuard)
-  async getUserInfo(@Req() req): Promise<UserForViewDto> {
-    const userInfo: ReturnUserDto = req.user;
-    return this.userService.getUserInfo(userInfo);
+  async getUserInfo(
+    @Req() req,
+    @Res() res
+    ): Promise<UserForViewDto> {
+    try {
+      const userInfo: ReturnUserDto = req.user;
+      const data: UserForViewDto = await this.userService.getUserInfo(userInfo);
+      return res.status(statusCode.OK)
+        .send(util.success(
+          statusCode.OK,
+          message.READ_USER_SUCCESS,
+          data
+        ))
+    } catch (error) {
+      logger.error(error);
+      return res.status(statusCode.INTERNAL_SERVER_ERROR)
+        .send(util.fail(
+          statusCode.INTERNAL_SERVER_ERROR,
+          message.INTERNAL_SERVER_ERROR
+        ))
+    }
   }
 
   @Post('/favorite/:newsId')
