dc93ec9cc7240581194c81409d3e734e77615a35
Woojin Lee <dhkep03@gmail.com>
2022-10-16 02:44:16 +0900

    feat: design memo entity

---
+++ src/script/entity/memo.entity.ts
@@ -0,0 +1,20 @@
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Script } from "./script.entity";
+
+@Entity()
+export class Memo extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Script, (script) => script.memos, {
+    onDelete: 'CASCADE',
+  })
+  script: Script;
+
+  @Column()
+  order: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  content: string;
+}
