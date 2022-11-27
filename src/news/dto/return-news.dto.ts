bf9953a85b49952dc7b713a35b726e91d192d5de
Woojin Lee <dhkep03@gmail.com>
2022-11-27 23:20:55 +0900

    feat: return news dto에 haveGuide 추가

--- src/news/dto/return-news.dto.ts
+++ src/news/dto/return-news.dto.ts
@@ -1,3 +1,4 @@
+import { ScriptGuide } from "src/dummy/entity/script-guide.entity";
 import { Time } from "src/modules/Time";
 import { Tag } from "src/tag/tag.entity";
 import { Category } from "../common/category.enum";
@@ -21,20 +22,33 @@ export class ReturnNewsDto {
     this.isEmbeddable = news.isEmbeddable;
     this.reportDate = news.reportDate;
     this.isFavorite = false;
+    this.checkHaveGuide(news);
     this.tagsForView = news.tagsForView;
-}
-    id: number;
-    title: string;
-    category: Category;
-    announcerGender: Gender;
-    channel: Channel;
-    link: string;
-    thumbnail: string;
-    startTime: number;
-    endTime: number;
-    suitability: Suitability;
-    isEmbeddable: boolean;
-    reportDate: Date;
-    isFavorite: boolean;
-    tagsForView?: Tag[];
+  }
+  id: number;
+  title: string;
+  category: Category;
+  announcerGender: Gender;
+  channel: Channel;
+  link: string;
+  thumbnail: string;
+  startTime: number;
+  endTime: number;
+  suitability: Suitability;
+  isEmbeddable: boolean;
+  reportDate: Date;
+  isFavorite: boolean;
+  tagsForView?: Tag[];
+  haveGuide: boolean;
+
+  async checkHaveGuide(news: News): Promise<void> {
+    const scriptGuide: ScriptGuide = await news.scriptGuide;
+    if (!scriptGuide) {
+      this.haveGuide = false
+      console.log("haveGuide is false");
+      return;
+    }
+    console.log("haveGuide is true");
+    this.haveGuide = true;
+  }
 }
--- src/news/utils/change-return-news-list-to-dto.ts
+++ src/news/utils/change-return-news-list-to-dto.ts
@@ -5,7 +5,7 @@ import { News } from "../news.entity"
 export const changeReturnNewsListToDto = (newsList: News[]): ReturnNewsDtoCollection => {
   const returnNewsDtoList: ReturnNewsDto[] = newsList.map(
     (news) => new ReturnNewsDto(news)
-  )
+    )
   const returnNewsDtoCollection: ReturnNewsDtoCollection = new ReturnNewsDtoCollection(returnNewsDtoList)
   return returnNewsDtoCollection;
 }
