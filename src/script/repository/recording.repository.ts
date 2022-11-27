0b893a5752b8b286f25bdf2321e2d30ed329201d
Sigrid <sigrid.jinhyung@gmail.com>
2022-11-27 20:10:27 +0900

    refactor: repo saved script activiated

--- src/script/repository/recording.repository.ts
+++ src/script/repository/recording.repository.ts
@@ -1,13 +1,13 @@
 import { EntityRepository, Repository } from "typeorm";
 import { Recording } from "../entity/recording.entity";
 import { RecordingDto } from "../dto/recording.dto";
-import { ReturnRecordingDto } from "../dto/return-recording.dto";
+// import { ReturnRecordingDto } from "../dto/return-recording.dto";
 
 @EntityRepository(Recording)
 export class RecordingRepository extends Repository<Recording> {
   async createRecording(
     recordingDto: RecordingDto,
-  ): Promise<ReturnRecordingDto> {
+  ): Promise<Recording> {
     const recording: Recording = new Recording();
     recording.name = recordingDto.name;
     recording.link = recordingDto.link;
