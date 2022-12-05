cea27df22159cfb73fe213e82090e1f6e178bd65
Woojin Lee <dhkep03@gmail.com>
2022-12-05 21:20:41 +0900

    feat: implement create history

--- src/history/history.service.ts
+++ src/history/history.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { News } from 'src/news/news.entity';
+import { NewsRepository } from 'src/news/news.repository';
+import { User } from 'src/user/user.entity';
+import { HistoryRepository } from './history.repository';
 
 @Injectable()
-export class HistoryService {}
+export class HistoryService {
+  constructor(
+    @InjectRepository(HistoryRepository)
+    private historyRepository: HistoryRepository,
+    @InjectRepository(NewsRepository)
+    private newsRepository: NewsRepository,
+  ) {};
+
+  async createHistory(user: User, newsId: number): Promise<void> {
+    const news: News = await this.newsRepository.getNewsById(newsId);
+    await this.historyRepository.createHistory(user, news);
+  }
+}
