3158c71d17a3a9c991f03a49c2b1737e479933a3
Woojin Lee <dhkep03@gmail.com>
2022-12-05 21:20:55 +0900

    feat: add import in history module

--- src/history/history.module.ts
+++ src/history/history.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NewsRepository } from 'src/news/news.repository';
 import { HistoryController } from './history.controller';
+import { HistoryRepository } from './history.repository';
 import { HistoryService } from './history.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      HistoryRepository, NewsRepository,
+    ]),
+  ],
   controllers: [HistoryController],
   providers: [HistoryService]
 })
