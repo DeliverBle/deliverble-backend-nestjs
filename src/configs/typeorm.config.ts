35f4b7f0015f72974ce2e86d23c035d8700f8285
Sigrid <sigrid.jinhyung@gmail.com>
2022-12-13 11:01:18 +0900

    chore: typeORM config change to develop branch originals

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,13 +1,23 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 require("dotenv").config();
 
+// TEST CONFIG
+// type: 'mysql',
+//     host: process.env.DB_HOST,
+//     port: parseInt(process.env.DBPORT),
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DATABASE,
+//     charset: 'utf8mb4',
+//     entities: [__dirname + '/../**/*.entity.{js,ts}'],
+//     synchronize: true,
 
 export const typeORMConfig: TypeOrmModuleOptions = {
     type: 'mysql',
-    host: process.env.DB_HOST,
+    host: process.env.HOST,
     port: parseInt(process.env.DBPORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
     database: process.env.DATABASE,
     charset: 'utf8mb4',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
